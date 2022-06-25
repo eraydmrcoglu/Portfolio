4b2624f43e768fbb973db6078d7d44e9f1f5187e
Eray Demircioğlu <75967993+eraydmrcoglu@users.noreply.github.com>
2022-06-25 08:35:58 +0300

    Add files via upload

---
+++ components/data.js
@@ -0,0 +1,47 @@
+export const projectsList = [
+  {
+    id: 1,
+    name: "BMI Calculator",
+    description:
+      "Body mass index is calculated by dividing body mass by the square of the height in meters. This makes the calculation easy on the site I made.",
+    image: "/images/Bmi_Calculator_Screenshot.png",
+    link: "https://bmicalculatorr.vercel.app",
+    GitHub: "https://github.com/eraydmrcoglu/Projects/tree/main/bmicalculator",
+    project: "bmi-calculator",
+    tools: ["React"],
+  },
+  {
+    id: 2,
+    name: "Crypto App",
+    description:
+      "It is a site where we can instantly follow the current crypto currency values ​​in USDT / TRY.",
+    image: "/images/Crypto_App_Screenshot.png",
+    link: "https://crypto-app-binance.vercel.app",
+    GitHub: "https://github.com/eraydmrcoglu/Projects/tree/main/crypto-app",
+    project: "crypto-app",
+    tools: ["React"],
+  },
+  {
+    id: 3,
+    name: "Rock Paper Scissors Game",
+    description: "Rock-Paper-Scissors game played against the computer.",
+    image: "/images/Rock_Paper_Scissors_Screenshot.png",
+    link: "https://rock-paper-scissors-game1.vercel.app",
+    GitHub:
+      "https://github.com/eraydmrcoglu/Projects/tree/main/rock-paper-scissors-game",
+    project: "rock-paper-scissors-game",
+    tools: ["react"],
+  },
+  {
+    id: 4,
+    name: "Typing Speed App",
+    description:
+      "Today, there may be jobs where software workers need to type fast. Thanks to this application, he can gain the habit of typing fast by aiming to write the words that will come randomly in 60 seconds and reach the highest score.",
+    image: "/images/Typing_Speed_Screenshot.png",
+    link: "https://typing-speed-app.vercel.app",
+    GitHub:
+      "https://github.com/eraydmrcoglu/Projects/tree/main/typing-speed-app",
+    project: "typing-speed",
+    tools: ["React"],
+  },
+];
